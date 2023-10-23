e5a68ae129ed8e5e0373936c9a47162de8d55295
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-10-23 01:06:03 -0400

    Update main.rs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,12 +1,11 @@
 extern crate csv;
 extern crate rayon;
 extern crate rusty_machine;
-extern crate chrono;
 extern crate xlsxwriter;
 
 use std::error::Error;
-use std::fs::File;
-use std::time::{Instant};
+use std::path::PathBuf;
+use std::time::Instant;
 use rayon::prelude::*;
 use rusty_machine::linalg::Vector;
 use xlsxwriter::*;
@@ -15,7 +14,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let start_time = Instant::now();
 
     // Read the dataset
-    let data_path = format!("{}/Data/winequality-red.csv", std::env::var("GITHUB_WORKSPACE").unwrap());
+    let data_path = PathBuf::from(std::env::var("GITHUB_WORKSPACE")?)
+        .join("Data")
+        .join("winequality-red.csv");
     let mut rdr = csv::Reader::from_path(data_path)?;
     let mut data: Vec<Vector<f64>> = vec![];
 
@@ -34,27 +35,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Write to Excel
     let workbook = Workbook::new("statistics_rust.xlsx");
-    let mut mean_worksheet = workbook.add_worksheet(Some("Mean")).unwrap();
-    let mut median_worksheet = workbook.add_worksheet(Some("Median")).unwrap();
-    let mut std_worksheet = workbook.add_worksheet(Some("Standard Deviation")).unwrap();
-    let mut time_worksheet = workbook.add_worksheet(Some("Time")).unwrap();
+    let mut mean_worksheet = workbook.add_worksheet(Some("Mean"))?;
+    let mut median_worksheet = workbook.add_worksheet(Some("Median"))?;
+    let mut std_worksheet = workbook.add_worksheet(Some("Standard Deviation"))?;
+    let mut time_worksheet = workbook.add_worksheet(Some("Time"))?;
 
     for (i, &val) in means.iter().enumerate() {
-        mean_worksheet.write_number(i as u16, 0, val, None).unwrap();
+        mean_worksheet.write_number(i as u16, 0, val, None)?;
     }
 
     for (i, &val) in medians.iter().enumerate() {
-        median_worksheet.write_number(i as u16, 0, val, None).unwrap();
+        median_worksheet.write_number(i as u16, 0, val, None)?;
     }
 
     for (i, &val) in std_devs.iter().enumerate() {
-        std_worksheet.write_number(i as u16, 0, val, None).unwrap();
+        std_worksheet.write_number(i as u16, 0, val, None)?;
     }
 
-    time_worksheet.write_string(0, 0, "Time taken (sec)", None).unwrap();
-    time_worksheet.write_number(0, 1, time_taken, None).unwrap();
+    time_worksheet.write_string(0, 0, "Time taken (sec)", None)?;
+    time_worksheet.write_number(0, 1, time_taken, None)?;
 
-    workbook.close().unwrap();
+    workbook.close()?;
 
     println!("Statistics generated and written to Excel in {:.2} seconds.", time_taken);
 
