4682f7b309cfaf9e9612e4d7f7a13505dc321de9
Gunel Aghakishiyeva <78721466+aghakishiyeva@users.noreply.github.com>
2023-10-23 01:11:56 -0400

    Update main.py

--- Python/src/main.py
+++ Python/src/main.py
@@ -17,14 +17,11 @@ def compute_stats_and_export():
     # Time taken to compute statistics
     time_taken = end_time - start_time
 
-    # Writing to Excel
-    with pd.ExcelWriter("statistics_python.xlsx") as writer:
-        mean.to_excel(writer, sheet_name="Mean")
-        median.to_excel(writer, sheet_name="Median")
-        std.to_excel(writer, sheet_name="Standard Deviation")
-        pd.Series({"Time taken (sec)": time_taken}).to_excel(writer, sheet_name="Time")
-
-    print(f"Statistics generated and written to Excel in {time_taken:.2f} seconds.")
+    # Printing results instead of writing to Excel
+    print("Mean:\n", mean)
+    print("\nMedian:\n", median)
+    print("\nStandard Deviation:\n", std)
+    print(f"\nStatistics computed in {time_taken:.2f} seconds.")
 
 if __name__ == "__main__":
     compute_stats_and_export()
